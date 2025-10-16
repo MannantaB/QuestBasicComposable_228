40515b98d44890a36eace4e31b45328e45444c52
MannantaB <mannanta18@gmail.com>
2025-10-16 14:34:30 +0700

    Menambahkan Column dengan padding atas sebesar 20.dp

--- app/src/main/java/com/example/Tataletak.kt
+++ app/src/main/java/com/example/Tataletak.kt
@@ -1,7 +1,12 @@
 package com.example
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun TataletakColumn(modifier: Modifier){}
+fun TataletakColumn(modifier: Modifier){
+    Column(modifier = modifier.padding(top = 20.dp)){  }
+}
