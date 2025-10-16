61fb18f747463f4020a0b4cba34c6b761e2f42b7
MannantaB <mannanta18@gmail.com>
2025-10-16 15:45:29 +0700

    Perbaikan code MainActivity.kt

--- app/src/main/java/com/example/praktikum1/MainActivity.kt
+++ app/src/main/java/com/example/praktikum1/MainActivity.kt
@@ -16,32 +16,14 @@ import com.example.praktikum1.ui.theme.Praktikum1Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Praktikum1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    TataletakBoxColumnRow(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Praktikum1Theme {
-        Greeting("Android")
     }
 }
